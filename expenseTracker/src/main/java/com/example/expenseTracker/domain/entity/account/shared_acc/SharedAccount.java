1216cf1b066bfd75497f6ec4993261db7bff759e
chenj926 <shendicjl@gmail.com>
2025-05-02 03:05:20 -0400

    incorporate getter and setter

--- expenseTracker/src/main/java/com/example/expenseTracker/domain/entity/account/shared_acc/SharedAccount.java
+++ expenseTracker/src/main/java/com/example/expenseTracker/domain/entity/account/shared_acc/SharedAccount.java
@@ -1,72 +1,51 @@
 package com.example.expenseTracker.domain.entity.account.shared_acc;
 
-
 import com.example.expenseTracker.domain.entity.account.Account;
+import lombok.Getter;
+import lombok.Setter;
 
-import java.util.*;
+import java.util.Set;
 
 /**
- * The SharedAccount class extends the UserAccount class to represent a shared account.
- * It includes additional functionality to manage user identifications associated with the shared account.
+ * Represents a shared account accessible by multiple users.
+ * Extends base Account and manages associated user identifications.
  *
- * @author Rita
- * @author Jessica
- * @author Eric
+ * Authors: Rita, Jessica, Eric
  */
+@Getter
+@Setter
 public class SharedAccount extends Account {
     private Set<String> sharedUserIdentifications;
 
     /**
-     * Constructs a SharedAccount object with the specified shared account identification.
-     * Initializes an empty set of shared user identifications.
-     *
-     * @param shareAccountIdentification the identification for the shared account
+     * Constructs a SharedAccount with shared user IDs and password.
      */
     public SharedAccount(String shareAccountIdentification, Set<String> userIds, String sharedAccountPassword) {
-        super(null, sharedAccountPassword, shareAccountIdentification); // SharedAccount have own shared account id
+        super(null, sharedAccountPassword, shareAccountIdentification);
         this.sharedUserIdentifications = userIds;
     }
 
+    /**
+     * Constructs a SharedAccount with full financial data and user IDs.
+     */
     public SharedAccount(String shareAccountIdentification, Set<String> userIds, String sharedAccountPassword,
                          float totalIncome, float totalOutflow, float totalBalance) {
         super(null, sharedAccountPassword, shareAccountIdentification,
-                totalIncome, totalOutflow, totalBalance); // SharedAccount have own shared account id
+                totalIncome, totalOutflow, totalBalance);
         this.sharedUserIdentifications = userIds;
     }
 
     /**
-     * Gets the set of shared user identifications associated with this shared account.
-     *
-     * @return a set of shared user identifications
-     */
-    public Set<String> getSharedUserIdentifications() {
-        return this.sharedUserIdentifications;
-    }
-
-    /**
-     * Adds a user identification to the set of shared user identifications.
-     *
-     * @param identification the user identification to be added
+     * Adds a user ID to the shared user set.
      */
     public void addUserIdentification(String identification) {
         sharedUserIdentifications.add(identification);
     }
 
     /**
-     * Removes a user identification from the set of shared user identifications.
-     *
-     * @param identification the user identification to be removed
+     * Removes a user ID from the shared user set.
      */
     public void removeUserIdentification(String identification) {
         sharedUserIdentifications.remove(identification);
-    }
-
-    /**
-     * Sets the shared user identifications for this shared account.
-     *
-     * @param sharedUserIdentifications the new set of shared user identifications
-     */
-    public void setSharedUserIdentifications(Set<String> sharedUserIdentifications) {
-        this.sharedUserIdentifications = sharedUserIdentifications;
     }
 }
--- expenseTracker/src/main/java/com/example/expenseTracker/domain/entity/account/user_acc/UserAccount.java
+++ expenseTracker/src/main/java/com/example/expenseTracker/domain/entity/account/user_acc/UserAccount.java
@@ -1,27 +1,25 @@
 package com.example.expenseTracker.domain.entity.account.user_acc;
 
 import com.example.expenseTracker.domain.entity.account.Account;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.HashSet;
 import java.util.Set;
 
 /**
- * The UserAccount class implements the Account interface.
- * This class represents a user account with details such as username, password, identification,
- * and a list of transactions. It also keeps track of the total income, total outflow, and total balance.
+ * Represents a user account with shared account functionality.
+ * Inherits basic account behavior and adds ability to manage shared accounts.
  *
- * @author Eric
- * @author Jessica
+ * Authors: Eric, Jessica
  */
+@Getter
+@Setter
 public class UserAccount extends Account {
     private Set<String> sharedAccounts;
+
     /**
-     * Constructs a UserAccount object with the specified username, password, and identification.
-     * Initializes the transaction list and sets the income, outflow, and balance to zero.
-     *
-     * @param username       the username of the user account
-     * @param password       the password of the user account
-     * @param identification the identification of the user account
+     * Constructs a UserAccount with empty shared accounts and zeroed balances.
      */
     public UserAccount(String username, String password, String identification) {
         super(username, password, identification);
@@ -29,15 +27,7 @@ public class UserAccount extends Account {
     }
 
     /**
-     * Constructs a UserAccount object with the specified username, password, identification,
-     * total income, total outflow, and total balance. Initializes the transaction list.
-     *
-     * @param username       the username of the user account
-     * @param password       the password of the user account
-     * @param identification the identification of the user account
-     * @param totalIncome    the total cumulated income of the user account
-     * @param totalOutflow   the total cumulated outflow of the user account
-     * @param totalBalance   the total cumulated balance of the user account
+     * Constructs a UserAccount with specified balances and empty shared accounts.
      */
     public UserAccount(String username, String password, String identification,
                        float totalIncome, float totalOutflow, float totalBalance) {
@@ -46,39 +36,14 @@ public class UserAccount extends Account {
     }
 
     /**
-     * Retrieves the set of shared account IDs associated with this user account.
-     *
-     * @return a {@link Set} of shared account IDs
-     */
-    public Set<String> getSharedAccounts() {
-        return this.sharedAccounts;
-    }
-
-    /**
-     * Sets the shared accounts for this account.
-     * <p>
-     * This method replaces the existing set of shared account IDs with the provided set.
-     * </p>
-     *
-     * @param sharedAccounts a {@link Set} of shared account IDs to be associated with this account
-     */
-    public void setSharedAccounts(Set<String> sharedAccounts) {
-        this.sharedAccounts = sharedAccounts;
-    }
-
-    /**
-     * Adds a shared account ID to the set of shared accounts associated with this account.
-     *
-     * @param sharedAccountId the ID of the shared account to be added
+     * Adds a shared account ID to this user account.
      */
     public void addSharedAccount(String sharedAccountId) {
         this.sharedAccounts.add(sharedAccountId);
     }
 
     /**
-     * Removes a shared account ID from the set of shared accounts associated with this account.
-     *
-     * @param sharedAccountId the ID of the shared account to be removed
+     * Removes a shared account ID from this user account.
      */
     public void removeSharedAccount(String sharedAccountId) {
         this.sharedAccounts.remove(sharedAccountId);
