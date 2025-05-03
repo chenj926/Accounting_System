17da41b0ab785dbf4b36f925419b6269bbfd9f7b
chenj926 <shendicjl@gmail.com>
2025-05-03 04:10:04 -0400

    login signup dto

--- expenseTracker/src/main/java/com/example/expenseTracker/adaptors/web/dto/LoginRequestDto.java
+++ expenseTracker/src/main/java/com/example/expenseTracker/adaptors/web/dto/LoginRequestDto.java
@@ -1,4 +1,12 @@
 package com.example.expenseTracker.adaptors.web.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class LoginRequestDto {
+    private String username;
+    private String email;
+    private String password;
 }
--- expenseTracker/src/main/java/com/example/expenseTracker/adaptors/web/dto/SignupRequestDto.java
+++ expenseTracker/src/main/java/com/example/expenseTracker/adaptors/web/dto/SignupRequestDto.java
@@ -1,4 +1,13 @@
 package com.example.expenseTracker.adaptors.web.dto;
 
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class SignupRequestDto {
+    private String username;
+    private String email;
+    private String password;
 }
