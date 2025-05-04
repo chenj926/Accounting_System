faa63fa58640e035524df1e1fa7037896673f04a
chenj926 <shendicjl@gmail.com>
2025-05-04 10:06:54 -0400

    Remove old src/main files

--- src/main/java/use_case/login/user_account/UserAccountLoginOutputBoundaryTemp.java
+++
@@ -1,8 +0,0 @@
-package use_case.login.user_account;
-
-import use_case.login.LoginOutputData;
-
-public interface UserAccountLoginOutputBoundaryTemp {
-    LoginOutputData prepareSuccessView(LoginOutputData outputData);
-    LoginOutputData prepareFailView(String errorMessage);
-}
